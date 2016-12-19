21b89318ba0eff858beaead97cb61de63aedc9b7
Luke Childs <lukechilds123@gmail.com>
2016-12-19 14:58:03 +0700

    Test passing in custom endpoint array

--- test/unit.js
+++ test/unit.js
@@ -29,3 +29,13 @@ test('Onionoo instance contains expected endpoints', t => {
 
   t.deepEqual(Object.keys(onionoo), expectedEndpoints)
 })
+
+test('Can pass in custom endpoint array', t => {
+  const endpoints = [
+    'foo',
+    'bar'
+  ]
+  const onionoo = new Onionoo({ endpoints })
+
+  t.deepEqual(Object.keys(onionoo), endpoints)
+})
