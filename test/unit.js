c2af0539e362fe3a510ab2cad02df0f732e8ae47
Luke Childs <lukechilds123@gmail.com>
2016-12-19 13:36:15 +0700

    Test Onionoo cannot be invoked without 'new'

--- test/unit.js
+++ test/unit.js
@@ -4,3 +4,8 @@ import Onionoo from '../'
 test('Onionoo is a function', t => {
   t.is(typeof Onionoo, 'function')
 })
+
+test('Onionoo cannot be invoked without \'new\'', t => {
+  t.throws(() => Onionoo())
+  t.notThrows(() => new Onionoo())
+})
