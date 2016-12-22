cb5f8e7d25388849d4caed72d839253f7d94a56c
Luke Childs <lukechilds123@gmail.com>
2016-12-22 16:33:55 +0700

    Test query string is built correctly

---
+++ test/queries.js
@@ -0,0 +1,18 @@
+import test from 'ava'
+import nock from 'nock'
+import data from './fixtures/data'
+import Onionoo from '../'
+
+test('Query string is built correctly', async t => {
+  const onionoo = new Onionoo()
+
+  const defaultEndpoint = data.defaultEndpoints[0]
+  const scope = nock(data.defaultBaseUrl)
+    .get(`/${defaultEndpoint}?foo=bar`)
+    .reply(200, data.dummyResponse)
+
+  const response = await onionoo[defaultEndpoint]({ foo: 'bar' })
+
+  t.deepEqual(response.body, data.dummyResponse)
+  t.truthy(scope.isDone())
+})
