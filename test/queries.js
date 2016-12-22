3f44ce19aa6d3b80cc0f7f48ca62430430705c3c
Luke Childs <lukechilds123@gmail.com>
2016-12-22 16:38:38 +0700

    Test ":" char isn't url encoded so filters work

--- test/queries.js
+++ test/queries.js
@@ -16,3 +16,17 @@ test('Query string is built correctly', async t => {
   t.deepEqual(response.body, data.dummyResponse)
   t.truthy(scope.isDone())
 })
+
+test('":" char isn\'t url encoded so filters work', async t => {
+  const onionoo = new Onionoo()
+
+  const defaultEndpoint = data.defaultEndpoints[0]
+  const scope = nock(data.defaultBaseUrl)
+    .get(`/${defaultEndpoint}?foo=key:value`)
+    .reply(200, data.dummyResponse)
+
+  const response = await onionoo[defaultEndpoint]({ foo: 'key:value' })
+
+  t.deepEqual(response.body, data.dummyResponse)
+  t.truthy(scope.isDone())
+})
