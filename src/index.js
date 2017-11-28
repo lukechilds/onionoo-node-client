f141fb8b9cc76ba48d8e8a3008b41f27e968928e
Luke Childs <lukechilds123@gmail.com>
2017-11-28 16:29:59 +0700

    Remove internal caching logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const querystring = require('querystring');
 const got = require('got');
-const cacheManager = require('cache-manager');
-const expired = require('expired');
-const deepAssign = require('deep-assign');
 const pkg = require('../package.json');
 
 class Onionoo {
@@ -21,13 +18,6 @@ class Onionoo {
 				'uptime'
 			]
 		}, options);
-		if (options.cache !== false) {
-			this.options.cache = cacheManager.caching(Object.assign({
-				store: 'memory',
-				ttl: 18000, // 5 hours
-				max: 500
-			}, options.cache));
-		}
 
 		// Return object containing endpoint methods
 		return this.options.endpoints.reduce((onionoo, endpoint) => {
@@ -46,75 +36,13 @@ class Onionoo {
 		let url = `${this.options.baseUrl}/${endpoint}`;
 		url += qs ? `?${qs}` : '';
 
-		// If caching is disabled, just make the request
-		if (!this.options.cache) {
-			return this.makeRequest(url);
-		}
-
-		// If caching is enabled, check for url in cache
-		return this.options.cache.get(url)
-			.then(cachedResult => {
-				const options = {};
-
-				// If we have it cached
-				if (cachedResult) {
-					// Return the cached entry if it's still valid
-					if (!expired(cachedResult.headers)) {
-						return cachedResult;
-
-						// If it's stale, add last-modified date to headers
-					} else if (cachedResult.headers['last-modified']) {
-						options.headers = {
-							'if-modified-since': cachedResult.headers['last-modified']
-						};
-					}
-				}
-
-				// Make a request
-				return this.makeRequest(url, options)
-					.then(response => {
-						// If we get a 304, fill in the body
-						if (response.statusCode === 304) {
-							response.body = cachedResult.body;
-						}
-
-						//  If we get a 200 or 304, cache it
-						if ([200, 304].includes(response.statusCode)) {
-							this.options.cache.set(url, response);
-						}
-
-						return response;
-					});
-			});
-	}
-
-	// Returns a promise for a request
-	makeRequest(url, options = {}) {
-		options = deepAssign({
+		// Make request
+		return got(url, {
 			headers: {
 				'user-agent': `onionoo-node-client v${pkg.version} (${pkg.homepage})`
-			}
-		}, options);
-
-		return got(url, options)
-			.catch(err => {
-				// Don't throw 304 responses
-				if (err.statusCode === 304) {
-					return err.response;
-				}
-				throw err;
-			})
-			.then(response => {
-				// Format response
-				response = {
-					statusCode: response.statusCode,
-					statusMessage: response.statusMessage,
-					headers: response.headers,
-					body: response.body && JSON.parse(response.body)
-				};
-
-				return response;
-			});
+			},
+			json: true
+		});
 	}
 }
 
